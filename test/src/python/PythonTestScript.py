a2251f425405f34af51df6567120624584b40ea5
Dion Amago <dion@transition9.com>
2016-06-07 10:31:38 -0700

    wip

---
+++ test/src/python/PythonTestScript.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#This is an example script to interact with the cloud-compute-cannon server
+
+from cloudcomputecannon import ccc_client
+
+ccc = ccc_client('docker.host:9000')
+
+
+
